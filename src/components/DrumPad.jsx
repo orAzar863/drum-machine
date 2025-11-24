31adc4d512999c39f187467fc936a875eff2306a
Or Azar <or.azar14@gmail.com>
2025-11-24 22:21:51 +0200

    fix speed errors

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -7,14 +7,19 @@ const DrumPad = ({ pad, onPlay, active }) => {
   const handlePlay = () => {
     //if connected to audio element, play the sound
     if (audioRef.current) {
+      audioRef.current.pause();
       audioRef.current.currentTime = 0;
-      audioRef.current.play();
+      audioRef.current.play().catch((e) => {
+        //ignore AbortError, log others
+        if (e.name !== 'AbortError') {
+          console.error('Audio play error:', e);
+        }
+      });
     }
     //update display content in app component
-    if (onPlay) {
-      onPlay(pad.sound.name);
+    (onPlay) ? onPlay(pad.sound.name): 0;
     }
-  };
+  
 
   return (
     <button
@@ -26,6 +31,6 @@ const DrumPad = ({ pad, onPlay, active }) => {
       <audio className="clip" id={pad.key} ref={audioRef} src={pad.sound.url} />
     </button>
   );
-}
-export default DrumPad;
+};
 
+export default DrumPad;
